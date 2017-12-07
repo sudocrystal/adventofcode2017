65737ccac0c37416137fe2986e798f9f8b440b1d
Crystal Hess <chess@shoreline.edu>
2017-12-06 23:17:28 -0800

    adds day04

---
+++ day04/one.py
@@ -0,0 +1,13 @@
+import fileinput
+
+total = 0
+
+for line in fileinput.input(files="input.txt"):
+    words = line.split()
+    total += 1
+    for word in words:
+        if words.count(word) > 1:
+            total -= 1
+            break
+    
+print(total)
